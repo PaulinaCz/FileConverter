494715b9ff1c0719cb0ce3bf1a34298f90d547db
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-12-17 20:51:18 +0000

    naive csv to xlsx converter impl

---
+++ src/main/java/CsvToXlsxConverter.java
@@ -0,0 +1,37 @@
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+
+import java.io.*;
+import java.util.stream.Stream;
+
+public class CsvToXlsxConverter implements Converter{
+
+    public final String TO_FORMAT = ".xlsx";
+    public final String FROM_FORMAT = ".csv";
+
+    @Override
+    public void convert(String filePath) throws IOException {
+
+        SXSSFWorkbook workbook = new SXSSFWorkbook();
+        SXSSFSheet sheet = workbook.createSheet();
+
+        BufferedReader br = new BufferedReader(new FileReader(filePath + FROM_FORMAT));
+        String currentLine;
+        int rowNumber = 0;
+
+        while ((currentLine = br.readLine()) != null ){
+            String[] nextLine = currentLine.split(",");
+            rowNumber++;
+            Row currentRow = sheet.createRow(rowNumber);
+            Stream.iterate(0, i -> i+1).limit(nextLine.length).forEach(
+                    i -> currentRow.createCell(i).setCellValue(nextLine[i])
+            );
+        }
+
+        FileOutputStream outputStream = new FileOutputStream(new File(filePath + TO_FORMAT));
+        workbook.write(outputStream);
+        outputStream.close();
+
+    }
+}
