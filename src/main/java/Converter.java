6ca97637f15c1f00f350428c1982ead313c458bb
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-12-18 23:13:40 +0000

    added conversion from byte array to Converter interface

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,9 +1,11 @@
-import java.io.IOException;
-
 public interface Converter {
 
 
     /*Convert File fileName
      */
-    void convert(String filePath) throws IOException;
+    void convert(String filePath);
+
+    /*Read file as byte array and convert
+    */
+    void convertFromByteArray(String filePath);
 }
